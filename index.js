c2a0ec7571cae5dd51c345333e7eafac85b88160
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-08 18:24:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-class boardMember(name, homeState, training){}
+class boardMember{
+  boardMembers(name, homeState, training){
+    this.name = name
+    this.homeState = homeState
+    this.training = training
+  }
+  
+}
+
+class ceo {
+   boardMembers(name, homeState, training){
+    this.name = name
+    this.homeState = homeState
+    this.training = training
+  }
+  
+  
+}
+
