601fbd4119e8df23b778257fce27dbcbfa9de3ca
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-08 18:33:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class boardMember{
-  boardMembers(name, homeState, training){
+  constructor(name, homeState, training){
     this.name = name
     this.homeState = homeState
     this.training = training
